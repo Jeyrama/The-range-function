477a923db99213f6a21ff5dc938d8e01efac3e11
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-03-28 12:58:54 -0700

    Update The-range-function.js

--- The-range-function.js
+++ The-range-function.js
@@ -28,3 +28,17 @@ Notes:
 
 
 // Solution
+
+function range(start, end, step) {
+  
+  let result = [],
+      a = arguments.length == 1 ? 0 : start,
+      b = arguments.length == 1 ? start : end,
+      c = step != void(0) ? step : 1;
+  if( b < a ) return result;
+  
+  for (let i = a; (step == 0 ? result.length + a : i) < b; i = i + c) {
+    result.push(i);
+  }
+  return result;
+}
